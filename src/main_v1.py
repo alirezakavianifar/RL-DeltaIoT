418c43253cb28998f6e5a7b061325294dc59ec35
Alireza Kavianifar <alireza2011110@gmail.com>
2024-07-20 04:28:53 +0330

    her

--- src/main_v1.py
+++ src/main_v1.py
@@ -4,7 +4,8 @@ import numpy as np
 import streamlit as st
 import shutil
 from collections import defaultdict
-from stable_baselines3 import DQN, PPO
+from stable_baselines3 import DQN, PPO, A2C
+from stable_baselines3.her import GoalSelectionStrategy, HerReplayBuffer
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
 from stable_baselines3.common.vec_env import DummyVecEnv
 from gymnasium.wrappers.time_limit import TimeLimit
@@ -19,11 +20,13 @@ from src.drl4sao.stable_algos.custom_policies.bayesian_ucb import BayesianUCB
 from src.drl4sao.stable_algos.rl_algos.a2c import A2C
 from concurrent.futures import ProcessPoolExecutor
 
+
 def main():
     st.title("RL Environment Configuration")
 
     # Input parameters
-    env_name = st.selectbox("Environment Name", ['DeltaIoTv1', 'DeltaIoTv2', 'BDBC_AllNumeric'])
+    env_name = st.selectbox("Environment Name", [
+                            'DeltaIoTv1', 'DeltaIoTv2', 'BDBC_AllNumeric'])
     # Additional parameters for DeltaIoT environments
     additional_params = {}
     additional_params['max_episode_steps'] = MAX_EPISODE_STEPS[env_name]
@@ -31,9 +34,12 @@ def main():
         strategy_type = st.selectbox("Strategy Type", ['min', 'multi'])
         additional_params['strategy_type'] = strategy_type
         if strategy_type == 'multi':
-            additional_params['energy_coef'] = st.number_input("Energy Coefficient", min_value=0.0, max_value=1.0, value=0.1)
-            additional_params['packet_coef'] = st.number_input("Packet Coefficient", min_value=0.0, max_value=1.0, value=0.8)
-            additional_params['latency_coef'] = st.number_input("Latency Coefficient", min_value=0.0, max_value=1.0, value=0.8)
+            additional_params['energy_coef'] = st.number_input(
+                "Energy Coefficient", min_value=0.0, max_value=1.0, value=0.1)
+            additional_params['packet_coef'] = st.number_input(
+                "Packet Coefficient", min_value=0.0, max_value=1.0, value=0.8)
+            additional_params['latency_coef'] = st.number_input(
+                "Latency Coefficient", min_value=0.0, max_value=1.0, value=0.8)
             additional_params['setpoint_thresh'] = {
                 'lower_bound': st.number_input("Setpoint Threshold Lower Bound", 12.8),
                 'upper_bound': st.number_input("Setpoint Threshold Upper Bound", 13.0)
@@ -42,15 +48,18 @@ def main():
             goal = st.selectbox("Goal", ['energy', 'packet_loss', 'latency'])
             additional_params['goal'] = goal
 
-    algo_name = st.selectbox("Algorithm Name", ['DQN', 'PPO', 'A2C'])
+    algo_name = st.selectbox(
+        "Algorithm Name", ['DQN', 'PPO', 'A2C', 'HER_DQN'])
     policy = st.selectbox("Policy", ["MlpPolicy", 'BoltzmannPolicy', 'SoftmaxDQNPolicy', 'BoltzmannDQNPolicy', 'UCBDQNPolicy',
-                                    'UCB1TUNEDDQNPolicy', 'BayesianUCBDQNPolicy', 'SoftmaxUCBDQNPolicy', 'SoftmaxUCBAdaptiveDQNPolicy'])
+                                     'UCB1TUNEDDQNPolicy', 'BayesianUCBDQNPolicy', 'SoftmaxUCBDQNPolicy', 'SoftmaxUCBAdaptiveDQNPolicy'])
     lr = st.text_input("Learning Rate", "0.0001,0.001,0.01,0.1")
-    exploration_fraction = st.text_input("Exploration Fraction (for DQN)", "0.1,0.2,0.4,0.6")
+    exploration_fraction = st.text_input(
+        "Exploration Fraction (for DQN)", "0.1,0.2,0.4,0.6")
     gamma = st.number_input("Gamma", min_value=0.0, max_value=1.0,  value=0.99)
     batch_size = st.number_input("Batch Size", 64)
     total_timesteps = st.number_input("Total Timesteps", 15000)
-    max_episode_steps = st.number_input("Max Episode Steps", additional_params['max_episode_steps'])
+    max_episode_steps = st.number_input(
+        "Max Episode Steps", additional_params['max_episode_steps'])
     chkpt_dir = st.text_input("Checkpoint Directory", "models")
     log_path = st.text_input("Log Path", "logs")
     execution_mode = st.selectbox("Execution Mode", ['Serial', 'Parallel'])
@@ -58,7 +67,8 @@ def main():
     # Number of processes input for parallel execution
     num_processes = None
     if execution_mode == 'Parallel':
-        num_processes = st.number_input("Number of Processes", min_value=1, value=4)
+        num_processes = st.number_input(
+            "Number of Processes", min_value=1, value=4)
 
     # Option to delete the contents of logs and models folders
     if st.button("Clear Logs and Models"):
@@ -90,12 +100,14 @@ def main():
                 'n_obs_space': n_obs_space,
                 'max_episode_steps': max_episode_steps
             })
-            stable_dqn(env_name, algo_name, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params, execution_mode, num_processes)
+            stable_dqn(env_name, algo_name, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, chkpt_dir,
+                       log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params, execution_mode, num_processes)
             st.success("Model training completed")
         except PermissionError as e:
             st.error(f"PermissionError: {e}")
         except Exception as e:
             st.error(f"An error occurred: {e}")
+
 
 def get_env_parameters(env_name):
     if env_name in ['DeltaIoTv1', 'DeltaIoTv2']:
@@ -106,38 +118,41 @@ def get_env_parameters(env_name):
         n_actions = 100
     return n_obs_space, n_actions
 
+
 def setup_env(env_name, additional_params):
     total_timesteps = additional_params.get('total_timesteps', 512)
     n_actions = additional_params.get('n_actions', 216)
     n_obs_space = additional_params.get('n_obs_space', 3)
     max_episode_steps = additional_params.get('max_episode_steps', 216)
-    
+
     if env_name in ['DeltaIoTv1', 'DeltaIoTv2']:
         env = Monitor(TimeLimit(DeltaIotEnv(
-            data_dir=glob.glob(os.path.join(f'data/{env_name}/train', "*.json")),
-            timesteps=total_timesteps, 
-            n_actions=n_actions, 
+            data_dir=glob.glob(os.path.join(
+                f'data/{env_name}/train', "*.json")),
+            timesteps=total_timesteps,
+            n_actions=n_actions,
             n_obs_space=n_obs_space,
-            reward_type=RewardStrategy(additional_params.get('strategy_type')), 
+            reward_type=RewardStrategy(additional_params.get('strategy_type')),
             goal=additional_params.get('goal'),
             energy_coef=additional_params.get('energy_coef', 0.0),
-            packet_coef=additional_params.get('packet_coef', 0.0), 
-            latency_coef=additional_params.get('latency_coef', 0.0), 
-            packet_thresh=10.0, 
-            latency_thresh=5.0, 
-            energy_thresh=12.9, 
+            packet_coef=additional_params.get('packet_coef', 0.0),
+            latency_coef=additional_params.get('latency_coef', 0.0),
+            packet_thresh=10.0,
+            latency_thresh=5.0,
+            energy_thresh=12.9,
             setpoint_thresh=additional_params.get('setpoint_thresh', 0.0)
         ), max_episode_steps=max_episode_steps))
     elif env_name == 'BDBC_AllNumeric':
         env = Monitor(TimeLimit(BDBC_AllNumeric(
             data_dir='data/BDBC_AllNumeric',
             timesteps=100,
-            n_actions=n_actions, 
-            n_obs_space=n_obs_space, 
+            n_actions=n_actions,
+            n_obs_space=n_obs_space,
             performance_thresh=39.0
         ), max_episode_steps=max_episode_steps))
-    
+
     return DummyVecEnv([lambda: env])
+
 
 def stable_dqn(env_name, algo_name, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params, execution_mode, num_processes=None):
     lrs = lr.split(',')
@@ -148,28 +163,41 @@ def stable_dqn(env_name, algo_name, policy, lr, exploration_fraction, gamma, bat
         with ProcessPoolExecutor(max_workers=num_processes) as executor:
             for lr in lrs:
                 for exploration_fraction in exploration_fractions:
-                    tasks.append(executor.submit(train_model, env_name, algo_name, policy, float(lr), float(exploration_fraction), gamma, batch_size, total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params))
-        
+                    tasks.append(executor.submit(train_model, env_name, algo_name, policy, float(lr), float(exploration_fraction), gamma,
+                                 batch_size, total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params))
+
         for task in tasks:
             task.result()
 
     else:
         for lr in lrs:
             for exploration_fraction in exploration_fractions:
-                train_model(env_name, algo_name, policy, float(lr), float(exploration_fraction), gamma, batch_size, total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params)
+                train_model(env_name, algo_name, policy, float(lr), float(exploration_fraction), gamma, batch_size,
+                            total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params)
+
 
 def train_model(env_name, algo_name, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, chkpt_dir, log_path, warmup_count, eps_min, epsilon, num_pulls, additional_params):
     env = setup_env(env_name, additional_params)
-    log_path_base = os.path.join(log_path, f"{env_name}-policy={policy}-lr={lr}-batch_size={batch_size}-gamma={gamma}-total_timesteps={total_timesteps}-exploration_fraction={exploration_fraction}")
-    checkpoint_callback = CheckpointCallback(save_freq=1, save_path=chkpt_dir, name_prefix=f"{env_name}-policy={policy}-lr={lr}-batch_size={batch_size}-gamma={gamma}-total_timesteps={total_timesteps}-exploration_fraction={exploration_fraction}")
-    eval_callback = EvalCallback(env, callback_on_new_best=checkpoint_callback, eval_freq=int(total_timesteps / 20), verbose=1, n_eval_episodes=5)
+    log_path_base = os.path.join(log_path, f"{env_name}-policy={policy}-lr={lr}-batch_size={batch_size}-gamma={
+                                 gamma}-total_timesteps={total_timesteps}-exploration_fraction={exploration_fraction}")
+    checkpoint_callback = CheckpointCallback(save_freq=1, save_path=chkpt_dir, name_prefix=f"{env_name}-policy={policy}-lr={
+                                             lr}-batch_size={batch_size}-gamma={gamma}-total_timesteps={total_timesteps}-exploration_fraction={exploration_fraction}")
+    eval_callback = EvalCallback(env, callback_on_new_best=checkpoint_callback, eval_freq=int(
+        total_timesteps / 20), verbose=1, n_eval_episodes=5)
 
     if algo_name == "DQN":
-        train_dqn(env, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, warmup_count, eps_min, epsilon, num_pulls, additional_params.get('setpoint_thresh'), log_path_base, checkpoint_callback, eval_callback)
+        train_dqn(env, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, warmup_count, eps_min,
+                  epsilon, num_pulls, additional_params.get('setpoint_thresh'), log_path_base, checkpoint_callback, eval_callback)
     elif algo_name == "PPO":
-        train_ppo(env, policy, lr, gamma, batch_size, total_timesteps, log_path_base, checkpoint_callback, eval_callback)
+        train_ppo(env, policy, lr, gamma, batch_size, total_timesteps,
+                  log_path_base, checkpoint_callback, eval_callback)
     elif algo_name == "A2C":
-        train_a2c(env, policy, lr, gamma, total_timesteps, log_path_base, checkpoint_callback, eval_callback)
+        train_a2c(env, policy, lr, gamma, total_timesteps,
+                  log_path_base, checkpoint_callback, eval_callback)
+    elif algo_name == "HER_DQN":
+        train_her_dqn(env, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, warmup_count, eps_min,
+                      epsilon, num_pulls, additional_params.get('setpoint_thresh'), log_path_base, checkpoint_callback, eval_callback)
+
 
 def train_dqn(env, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, warmup_count, eps_min, epsilon, num_pulls, setpoint_thresh, log_path, checkpoint_callback, eval_callback):
     bayesian_ucb = BayesianUCB(env.action_space.n)
@@ -190,7 +218,9 @@ def train_dqn(env, policy, lr, exploration_fraction, gamma, batch_size, total_ti
                       num_pulls=num_pulls,
                       setpoint_thresh=setpoint_thresh,
                       bayesian_ucb=bayesian_ucb)
-    model.learn(total_timesteps=total_timesteps, log_interval=1, callback=eval_callback)
+    model.learn(total_timesteps=total_timesteps,
+                log_interval=1, callback=eval_callback)
+
 
 def train_ppo(env, policy, lr, gamma, batch_size, total_timesteps, log_path, checkpoint_callback, eval_callback):
     model = PPO(policy,
@@ -201,7 +231,9 @@ def train_ppo(env, policy, lr, gamma, batch_size, total_timesteps, log_path, che
                 tensorboard_log=log_path,
                 device='cuda',
                 verbose=1)
-    model.learn(total_timesteps=total_timesteps, log_interval=1, callback=eval_callback)
+    model.learn(total_timesteps=total_timesteps,
+                log_interval=1, callback=eval_callback)
+
 
 def train_a2c(env, policy, lr, gamma, total_timesteps, log_path, checkpoint_callback, eval_callback):
     model = A2C(policy,
@@ -211,7 +243,33 @@ def train_a2c(env, policy, lr, gamma, total_timesteps, log_path, checkpoint_call
                 tensorboard_log=log_path,
                 device='cuda',
                 verbose=1)
-    model.learn(total_timesteps=total_timesteps, log_interval=1, callback=eval_callback)
+    model.learn(total_timesteps=total_timesteps,
+                log_interval=1, callback=eval_callback)
+
+
+def train_her_dqn(env, policy, lr, exploration_fraction, gamma, batch_size, total_timesteps, warmup_count, eps_min, epsilon, num_pulls, setpoint_thresh, log_path, checkpoint_callback, eval_callback):
+    # Example strategy (can be 'future', 'final', 'episode')
+    goal_selection_strategy = 'future'
+    model = CustomDQN(policy,
+                      env,
+                      replay_buffer_class=HerReplayBuffer,  # Use HER replay buffer
+                      replay_buffer_kwargs=dict(
+                          n_sampled_goal=4,
+                          goal_selection_strategy=goal_selection_strategy,
+                      ),
+                      learning_rate=lr,
+                      gamma=gamma,
+                      batch_size=batch_size,
+                      learning_starts=warmup_count,
+                      exploration_initial_eps=epsilon,
+                      exploration_final_eps=eps_min,
+                      exploration_fraction=exploration_fraction,
+                      tensorboard_log=log_path,
+                      device='cuda',
+                      verbose=1)
+    model.learn(total_timesteps=total_timesteps,
+                log_interval=1, callback=eval_callback)
+
 
 if __name__ == "__main__":
     main()
